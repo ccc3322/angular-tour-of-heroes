6a493cceb49f127ba4a6d772c1a941c3a2b44a6c
ccc3322 <ccc3322@rit.edu>
2026-02-13 15:40:15 -0500

    print hero name... doesn't work

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,11 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-heroes',
-  standalone: true,
-  imports: [],
   templateUrl: './heroes.component.html',
-  styleUrl: './heroes.component.css'
+  styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent {
-
+  hero = 'Windstorm';
 }
